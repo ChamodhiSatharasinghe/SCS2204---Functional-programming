f783f4bc72bb254d25eba8da09d92c008847377d
Chimi <you@example.com>
2020-07-19 23:40:46 +0530

    XYZ error fixed

--- Assignment 4/caeserCipher.scala
+++ Assignment 4/caeserCipher.scala
@@ -2,11 +2,11 @@ object caeserCipher extends App{
 
 val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-var s="HELLO CHAMODHI"
+var s="MY NAME IS CHAMODHI DEWWANDI"
 
 val E=(c:Char,key:Int,a:String) => if(c==' ') ' ' else a((a.indexOf(c.toUpper)+key)%a.size)
 
-val D=(c:Char,key:Int,a:String) => if(c==' ') ' ' else a((a.indexOf(c.toUpper)-key)%a.size)
+val D=(c:Char,key:Int,a:String) => if(c==' ') ' ' else a(if(((a.indexOf(c.toUpper)-key)%a.size)<0) 26+(a.indexOf(c.toUpper)-key)%a.size else (a.indexOf(c.toUpper)-key)%a.size )
 
 val cipher=(algo:(Char,Int,String) => Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
 
