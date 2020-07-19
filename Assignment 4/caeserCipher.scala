960db311a67f78ffb8d995b87941d7579ecccffa
Chimi <you@example.com>
2020-07-19 22:50:54 +0530

    assignment 4

---
+++ Assignment 4/caeserCipher.scala
@@ -0,0 +1,22 @@
+object caeserCipher extends App{
+
+val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var s="MYNAMEISCHAMODHI"
+
+val E=(c:Char,key:Int,a:String) => a((a.indexOf(c.toUpper)+key)%a.size)
+
+val D=(c:Char,key:Int,a:String) => a((a.indexOf(c.toUpper)-key)%a.size)
+
+val cipher=(algo:(Char,Int,String) => Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
+
+val ct=cipher(E,s,5,alphabet)
+
+val pt=cipher(D,ct,5,alphabet)
+
+println(s)
+
+println(ct)
+
+println(pt)
+}
